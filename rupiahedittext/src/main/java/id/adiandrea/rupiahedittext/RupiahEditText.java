6793e3c0f9630d66a1987fc0fdfbd588825f8f58
Adi Andrea <dekz.itfz@gmail.com>
2020-01-06 10:28:29 +0800

    convert to java

---
+++ rupiahedittext/src/main/java/id/adiandrea/rupiahedittext/RupiahEditText.java
@@ -0,0 +1,86 @@
+package id.adiandrea.rupiahedittext;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatEditText;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class RupiahEditText extends AppCompatEditText {
+
+    private RupiahEditText editText = RupiahEditText.this;
+
+    public RupiahEditText(Context context) {
+        super(context);
+        init();
+    }
+
+    public RupiahEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public RupiahEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void init(){
+
+        editText.setText("0");
+        editText.setRawInputType(InputType.TYPE_CLASS_NUMBER);
+
+        this.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                editText.removeTextChangedListener(this);
+                if(!s.toString().equals("")){
+                    if(s.toString().substring(0, 1).equals("0")){
+                        //angka pertama 0
+                        if(s.toString().length() > 1){
+                            //panjang angka lebih dari 1
+                            if(s.toString().substring(0, 1).equals("0") && s.toString().substring(1,2) != "0"){
+                                //angka pertama 0 dan angka kedua tidak 0, hilangkan angka 0 dikiri
+                                editText.setText(formatCurrency(Long.parseLong(new StringBuilder(s.toString()).deleteCharAt(0).toString())));
+                            }else if(!s.toString().substring(0, 1).equals("0")){
+                                //angka pertama tidak 0
+                                editText.setText(formatCurrency(Long.parseLong(s.toString())));
+                            }else{
+                                //angka kedua 0
+                                editText.setText("0");
+                            }
+                        }else{
+                            //panjang angka adalah 1
+                            editText.setText(formatCurrency(Long.parseLong(s.toString())));
+                        }
+                    }else{
+                        //angka pertama tidak 0
+                        String cleanString = s.toString().replace(".", "");
+                        editText.setText(formatCurrency(Long.parseLong(cleanString)));
+                    }
+                }else{
+                    editText.setText("0");
+                }
+
+                editText.setSelection(editText.length());
+                editText.addTextChangedListener(this);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+    }
+
+    private String formatCurrency(Long amount) {
+        NumberFormat format = NumberFormat.getNumberInstance(Locale.GERMANY);
+        return format.format(amount);
+    }
+}
